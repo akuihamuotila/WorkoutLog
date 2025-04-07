593739c03e22703fe5f7ae8cba802d857e0f6b46
akuihamuotila <aku.ihamuotila@myy.haaga-helia.fi>
2025-04-07 13:18:50 +0300

    Alustettu Spring Boot -projekti WorkoutLog

---
+++ src/main/java/fi/haagahelia/workoutlog/WorkoutlogApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.workoutlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/haagahelia/workoutlog/WorkoutlogApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.workoutlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
